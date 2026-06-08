c9ad210e53c890663a8599b20d7dd2078490f3ed
Damien Cletheroe <damiencletheroe@hotmail.co.uk>
2026-06-08 14:03:57 +0100

    2: Player class and player instance

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import pygame
 from logger import log_state
 from constants import *
+from player import Player
 
-def main():
+def main() -> None:
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0.0
+    
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
